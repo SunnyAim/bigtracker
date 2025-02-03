4d02894a15b6350bb4810dc6bfc5727ea0e58800
eatplastic <117698474+eatpIastic@users.noreply.github.com>
2025-02-03 14:27:53 -0600

    test

---
+++ PlayerObject.js
@@ -0,0 +1,252 @@
+import PogObject from "../PogData";
+
+export default class PlayerObject {
+    constructor(UUID, username="unknown", note="", dodge=false, dodgeLength=0, dodgeDate=0, numRuns=0, lastSession=Date.now(), avgDeaths=0, avgSSTime=0, avgSSTimeN=0,
+                pre4Rate=0, pre4RateN=0, ee3Rate=0, ee3RateN=0, avgRunTime=0, avgBr=0, avgBrN=0, avgCamp=0, avgCampN=0, avgTerms=0, avgTermsN=0,
+                ssPB=17, termsPB=80, runPB=1000, campPB=100) {
+        this.playerData = new PogObject("bigtracker/players", {
+            UUID: UUID,
+            USERNAME: username?.toLowerCase(),
+            NOTE: note,
+            DODGE: dodge,
+            DODGELENGTH: dodgeLength,
+            DODGEDATE: dodgeDate,
+            NUMRUNS: numRuns,
+            LASTSESSION: lastSession,
+            DEATHS: avgDeaths,
+            AVGSSTIME: avgSSTime,
+            AVGSSTIMEN: avgSSTimeN,
+            SSTRACKING: [],
+            PRE4RATE: pre4Rate,
+            PRE4RATEN: pre4RateN,
+            EE3RATE: ee3Rate,
+            EE3RATEN: ee3RateN,
+            AVGRUNTIME: avgRunTime,
+            AVGBR: avgBr,
+            AVGBRN: avgBrN,
+            AVGCAMP: avgCamp,
+            AVGCAMPN: avgCampN,
+            AVGTERMS: avgTerms,
+            AVGTERMSN: avgTermsN,
+            SSPB: ssPB,
+            TERMSPB: termsPB,
+            RUNPB: runPB,
+            CAMPPB: campPB
+        }, `${UUID}.json`);
+
+        this.save();
+    }
+
+    save() {
+        this.playerData.save();
+    }
+
+    printPlayer() {
+        ChatLib.chat(`${this.playerData.USERNAME}`);
+
+        if (this.playerData.NOTE !== "") ChatLib.chat(`${this.playerData.NOTE}`);
+
+        if (this.playerData.DODGE) {
+            let playerString = ""
+            if (this.playerData.DODGELENGTH !== 0) {
+                let timeLeft = Date.now() - this.playerData.DODGEDATE;
+                timeLeft /= 1000; // seconds
+                timeLeft /= 60; // minutes
+                timeLeft /= 60; // hours
+                timeLeft /= 24; // days
+                timeLeft = parseFloat( (this.playerData.DODGELENGTH - timeLeft).toFixed(1) );
+                playerString += `dodged; ${timeLeft} days remaining`;
+            } else {
+                playerString += `dodged`;
+            }
+            ChatLib.chat(playerString);
+        }
+
+        if (this.playerData.NUMRUNS !== 0) {
+            ChatLib.chat(`runs w/: ${this.playerData.NUMRUNS}`);
+            ChatLib.chat(`avg deaths: ${(this.playerData.DEATHS / this.playerData.NUMRUNS).toFixed(1)}`);
+            ChatLib.chat(`last run: ${(((Date.now() - this.playerData.LASTSESSION) / 1000) / 60 / 60 / 24).toFixed(1)} days ago`);
+            ChatLib.chat(`avg runtime: ${Math.trunc(this.playerData.AVGRUNTIME / 60)}m ${(this.playerData.AVGRUNTIME % 60).toFixed(1)}s`);
+            ChatLib.chat(`PBs: SS: ${this.playerData.SSPB} / RUN: ${this.playerData.RUNPB} / CAMP: ${this.playerData.CAMPPB} / TERMS: ${this.playerData.TERMSPB}`);
+        } else {
+            ChatLib.chat("no runs");
+        }
+
+        if (!this.playerData.SSTRACKING) {
+            this.playerData.SSTRACKING = [];
+            this.save();
+        }
+
+        if (this.playerData.SSTRACKING.length !== 0) {
+            ChatLib.chat(`ss median: ${this.getSSMedian()}`);
+        }
+
+        if (this.playerData.PRE4RATEN !== 0) {
+            ChatLib.chat(`pre4 rate: ${this.playerData.PRE4RATE}/${this.playerData.PRE4RATEN} (${((this.playerData.PRE4RATE / this.playerData.PRE4RATEN) * 100).toFixed(1)}%)`);
+        }
+
+        if (this.playerData.AVGBRN !== 0) {
+            ChatLib.chat(`avg br: ${this.playerData.AVGBR}`);
+        }
+
+        if (this.playerData.AVGCAMPN !== 0) {
+            ChatLib.chat(`avg camp: ${this.playerData.AVGCAMP}`);
+        }
+
+        if (this.playerData.AVGTERMSN !== 0) {
+            ChatLib.chat(`avg terms: ${this.playerData.AVGTERMS}`);
+        }
+    }
+
+    updateMovingAVG(TYPE, TYPEN, TIME, INCREMENT=true) {
+        this.playerData[TYPEN] += 1;
+        let newAvg = (this.playerData[TYPE] * (this.playerData[TYPEN] - 1) / this.playerData[TYPEN] + (TIME / this.playerData[TYPEN])).toFixed(2);
+        console.log(`${TYPE}: ${TIME}`);
+        this.playerData[TYPE] = parseFloat(newAvg);
+        this.save();
+
+        if (!INCREMENT) {
+            this.playerData[TYPEN] -= 1;
+            this.save();
+        }
+
+        switch (TYPE) {
+            case "AVGSSTIME": {
+                this.addSS(TIME);
+                if (TIME < this.playerData.SSPB) {
+                    this.playerData.SSPB = TIME;
+                    this.save();
+                }
+                break;
+            }
+            case "AVGRUNTIME": {
+                if (TIME < this.playerData.RUNPB) {
+                    this.playerData.RUNPB = TIME;
+                    this.save();
+                }
+                break;
+            }
+            case "AVGCAMP": {
+                if (TIME < this.playerData.CAMPPB) {
+                    this.playerData.CAMPPB = TIME;
+                    this.save();
+                }
+                break;
+            }
+            case "AVGTERMS": {
+                if (TIME < this.playerData.TERMSPB) {
+                    this.playerData.TERMSPB = TIME;
+                    this.save();
+                }
+                break;
+            }
+        }
+    }
+
+    dodge(length, note="") {
+        if (this.playerData.DODGE) {
+            this.playerData.DODGE = false;
+            this.playerData.DODGELENGTH = 0;
+            this.playerData.DODGEDATE = 0;
+            this.save();
+            ChatLib.chat(`no longer dodging ${this.playerData.USERNAME}`);
+            return;
+        }
+
+        if (!length || length === undefined) {
+            length = 0;
+        }
+
+        if (!note || note == undefined) {
+            note = "";
+        }
+
+        if (note != "") {
+            this.playerData.NOTE = note;
+        }
+
+        if (length != 0) {
+            this.playerData.DODGELENGTH = length;
+            this.playerData.DODGEDATE = Date.now();
+        }
+
+        this.playerData.DODGE = true;
+        this.save();
+
+        ChatLib.chat(`${this.playerData.USERNAME}${length == 0 ? "" : " " + length} ${note}`);
+
+    }
+
+    check(autokick=false, sayReason=false) {
+        this.printPlayer();
+        if(this.playerData.DODGE) {
+            World.playSound("mob.horse.donkey.idle", 1, 1)
+            let dodgeStr = "";
+            // ChatLib.chat(`${this.playerData.USERNAME} is dodged.`);
+            
+            if (this.playerData.DODGELENGTH !== 0) {
+                let timeLeft = Date.now() - this.playerData.DODGEDATE;
+                timeLeft /= 1000; // seconds
+                timeLeft /= 60; // minutes
+                timeLeft /= 60; // hours
+                timeLeft /= 24; // days
+                timeLeft = parseFloat( (this.playerData.DODGELENGTH - timeLeft).toFixed(1) );
+                if (timeLeft > 0) {
+                    dodgeStr = `: (dodged: ${timeLeft} days remaining)`;
+                } else {
+                    dodgeStr = ` was dodged for ${this.DODGELENGTH} days. removing dodge.`;
+                    this.playerData.DODGE = false;
+                    this.playerData.DODGELENGTH = 0;
+                    this.playerData.DODGEDATE = 0;
+                    this.save();
+                }
+            } else {
+                dodgeStr = ": (dodged)";
+            }
+
+            if (this.playerData.NOTE !== "") {
+                dodgeStr += ` : ${this.playerData.NOTE}`;
+            }
+
+
+            ChatLib.chat(`${this.playerData.USERNAME}${dodgeStr}`);
+            
+            if(this.playerData.DODGE && autokick) {
+                if (sayReason) {
+                    ChatLib.command(`pc ${this.playerData.USERNAME}${dodgeStr}`);
+                }
+
+                setTimeout( () => {
+                    ChatLib.command(`p kick ${this.playerData.USERNAME}`);
+                }, 500);
+            }
+        }
+    }
+
+    addSS(time) {
+        if (!this.playerData.SSTRACKING) {
+            this.playerData.SSTRACKING = [];
+        }
+
+        this.playerData.SSTRACKING.push(time);
+
+        if (this.playerData.SSTRACKING.length > 30) {
+            this.playerData.SSTRACKING.shift();
+        }
+
+        this.save();
+    }
+
+    getSSMedian() {
+        if (!this.playerData.SSTRACKING) {
+            this.playerData.SSTRACKING = [];
+            return 0.0;
+        }
+
+        let tempSS = [...this.playerData.SSTRACKING].sort((a, b) => a - b);
+        const half = Math.floor(tempSS.length / 2);
+
+        let val = (tempSS.length % 2 ? tempSS[half] : (tempSS[half - 1] + tempSS[half]) / 2);
+        return val.toFixed(2);
+    }
+}
---
+++ index.js
@@ -0,0 +1,536 @@
+/// <reference types="../CTAutocomplete" />
+
+import PogObject from "../PogData";
+import PlayerObject from "./PlayerObject";
+import Dungeon from "../BloomCore/dungeons/Dungeon";
+import request from "../requestV2";
+
+const S02PacketChat = Java.type("net.minecraft.network.play.server.S02PacketChat");
+const File = Java.type("java.io.File");
+
+const data = new PogObject("bigtracker", {
+    autoKick: false,
+    sayReason: false,
+    firstTime: true
+}, "settings.json");
+
+
+if (!FileLib.exists("./config/ChatTriggers/modules/bigtracker/players")) {
+    new File("./config/ChatTriggers/modules/bigtracker/players").mkdirs();
+}
+
+if (data.firstTime) {
+    if (FileLib.exists("./config/ChatTriggers/modules/bigtracker/list.json")) {
+        ChatLib.chat("list data found. importing to bigtracker");
+        try {
+            let fileData = FileLib.read("./config/ChatTriggers/modules/bigtracker/list.json");
+            fileData = JSON.parse(fileData)["list"];
+            for (let UUID in fileData) {
+                playerData[UUID] = new PlayerObject(UUID, fileData[UUID][0].toLowerCase(), fileData[UUID][1]);
+            }
+        } catch(e) {}
+    }
+
+    if (FileLib.exists("./config/ChatTriggers/modules/bigtracker/bigdata.json")) {
+        const tempData = new PogObject("bigtracker", {}, "bigdata.json");
+        let oldPlayerData = tempData["playerData"];
+        
+        for (let UUID in oldPlayerData) {
+            let oldPlayer = oldPlayerData[UUID];
+            console.log(`${oldPlayer?.["lastKnown"]}`);
+
+            new PlayerObject(UUID, oldPlayer?.["lastKnown"]?.toLowerCase(), oldPlayer?.["note"], oldPlayer?.["dodge"], oldPlayer?.["dodgeLength"],
+                oldPlayer?.["dodgeDate"], oldPlayer?.["numRuns"], oldPlayer?.["lastSession"], oldPlayer?.["avgDeaths"], oldPlayer?.["avgSSTime"],
+                oldPlayer?.["avgSSTimeN"], oldPlayer?.["pre4Rate"], oldPlayer?.["pre4RateN"], oldPlayer?.["ee3Rate"], oldPlayer?.["ee3RateN"],
+                oldPlayer?.["avgRunTime"], oldPlayer?.["avgBR"], oldPlayer?.["avgBRN"], oldPlayer?.["avgCamp"], oldPlayer?.["avgCampN"], oldPlayer?.["avgTerms"], oldPlayer?.["avgTermsN"]
+            )
+        }
+    }
+
+    data.firstTime = false;
+    data.save();
+
+    if (World.isLoaded()) {
+        ChatLib.command("big help", true);
+    }
+}
+
+
+const playerData = {};
+const namesToUUID = {};
+
+
+const getPlayerDataByUUID = (UUID, NAME) => {
+    if (playerData[UUID]) {
+        return playerData[UUID];
+    }
+
+    playerData[UUID] = new PlayerObject(UUID, NAME.toLowerCase());
+    return playerData[UUID];
+}
+
+
+const getPlayerDataByName = (NAME, makeRequest=true) => {
+    NAME = NAME.toLowerCase();
+
+    if (namesToUUID[NAME]) {
+        return getPlayerDataByUUID(namesToUUID[NAME], NAME);
+    }
+
+    if (!makeRequest) return;
+
+    request(`https://api.mojang.com/users/profiles/minecraft/${NAME}`)
+        .then(function(res) {
+            const UUID = JSON.parse(res).id;
+            NAME = JSON.parse(res).name?.toLowerCase();
+            namesToUUID[NAME] = UUID;
+            return getPlayerDataByUUID(UUID, NAME);
+        }
+    );
+}
+
+
+let partyMembers = {};
+let gotAllMembers = false;
+let runStart = 0;
+let campStart = 0;
+let termsStart = 0;
+let ssDone = false;
+let pre4Done = false;
+
+register("worldLoad", () => {
+    partyMembers = {};
+    gotAllMembers = false;
+    runStart = 0;
+    campStart = 0;
+    termsStart = 0;
+    ssDone = false;
+    pre4Done = false;
+});
+
+
+const getPartyMembers = () => {
+    if (!Dungeon.inDungeon) return;
+    if (gotAllMembers) return;
+
+    const Scoreboard = TabList?.getNames();
+    if (!Scoreboard || Scoreboard?.length === 0) return;
+
+    let numMembers = parseInt(Scoreboard[0]?.charAt(28));
+    let deadPlayer = false;
+    let tempPartyMembers = {};
+
+    for (let i = 1; i < Scoreboard.length; i++) {
+        if (Object.keys(tempPartyMembers).length === numMembers || Scoreboard[i].includes("Player Stats")) {
+            break;
+        }
+
+        if (Scoreboard[i].includes("[")) {
+            let line = Scoreboard[i].removeFormatting();
+            if(line?.includes("(DEAD)") || line?.includes("(EMPTY)")) {
+                deadPlayer = true;
+            }
+
+            let name = line.split(" ")?.[1].toLowerCase();
+
+            if (!namesToUUID[name]) {
+                getPlayerDataByName(name);
+            }
+            
+            let playerClass = "";
+            // console.log(line);
+
+            if (line.includes("(Healer")) playerClass = "Healer"
+            if (line.includes("(Archer")) playerClass = "Archer"
+            if (line.includes("(Mage")) playerClass = "Mage"
+            if (line.includes("(Tank")) playerClass = "Tank"
+            if (line.includes("(Berserk")) playerClass = "Berserk"
+
+
+            tempPartyMembers[name] = playerClass;
+        }
+    }
+
+    gotAllMembers = !deadPlayer;
+    partyMembers = tempPartyMembers;
+}
+
+
+register("packetReceived", (packet, event) => {
+    if (packet.func_148916_d()) return;
+
+    const chatComponent = packet.func_148915_c();
+    const text = new String(chatComponent.func_150254_d().removeFormatting());
+
+    if (text.match(/Party Finder > (.+) joined the dungeon group! .+/)) {
+        const match = text.match(/Party Finder > (.+) joined the dungeon group! .+/);
+        const name = match[1].toLowerCase();
+        let player = getPlayerDataByName(name);
+
+        if (!player) {
+            executeQueue.push([name, "check", Date.now()])
+        } else {
+            player.check(data.autoKick, data.sayReason);
+        }
+    }
+    else if (text == "[BOSS] Goldor: Who dares trespass into my domain?") {
+        termsStart = Date.now();
+        getPartyMembers();
+    }
+    else if (text.match(/\s+☠ Defeated Maxor, Storm, Goldor, and Necron in (\d+)m\s+(\d+)s/)) {
+        getPartyMembers();
+        const match = text.match(/\s+☠ Defeated Maxor, Storm, Goldor, and Necron in (\d+)m\s+(\d+)s/);
+        const time = (parseInt(match[1]) * 60) + parseInt(match[2]);
+
+        for (let name of Object.keys(partyMembers)) {
+            let player = getPlayerDataByName(name);
+
+            if (!player) {
+                executeQueue.push([name, "updateMovingAVG", Date.now(), "AVGRUNTIME", "NUMRUNS", time])
+            } else {
+                player.updateMovingAVG("AVGRUNTIME", "NUMRUNS", time);
+            }
+        }
+    }
+    else if (text.match(/☠(.+)/) && Dungeon.inDungeon && !(text.includes(" Defeated ") || text.includes("reconnected.") || text.includes(" disconnected "))) {
+        let name = text.split(" ")[2].toLowerCase();
+        let player = getPlayerDataByName(name);
+
+        if (!player) {
+            executeQueue.push([name, "DEATHS", Date.now()]);
+        } else {
+            player.playerData.DEATHS += 1;
+            player.save();
+        }
+    }
+    else if (text.startsWith("[BOSS] The Watcher:")) {
+        if (campStart === 0) {
+            campStart = Date.now();
+            let brTime = Date.now() - runStart;
+            brTime /= 1000;
+            console.log(`brTime: ${brTime}`);
+
+            if (brTime > 45) {
+                brTime = 45;
+            }
+
+            brTime = parseFloat( brTime.toFixed(2) );
+            getPartyMembers();
+
+            for (let name of Object.keys(partyMembers)) {
+                if (partyMembers[name] !== "Archer" && partyMembers[name] !== "Mage") {
+                    continue;
+                }
+                let player = getPlayerDataByName(name);
+
+                if (!player) {
+                    executeQueue.push([name, "updateMovingAVG", Date.now(), "AVGBR", "AVGBRN", brTime]);
+                } else {
+                    player.updateMovingAVG("AVGBR", "AVGBRN", brTime);
+                }
+            }
+        }
+
+        if (text == "[BOSS] The Watcher: You have proven yourself. You may pass.") {
+            let campTime = Date.now() - campStart;
+            campTime /= 1000;
+
+            if (campTime > 85) {
+                campTime = 85;
+            }
+
+            campTime = parseFloat( campTime.toFixed(2) );
+
+            getPartyMembers();
+            for (let name of Object.keys(partyMembers)) {
+                if (partyMembers[name] !== "Mage") {
+                    continue;
+                }
+                let player = getPlayerDataByName(name);
+                if (!player) {
+                    executeQueue.push([name, "updateMovingAVG", Date.now(), "AVGCAMP", "AVGCAMPN", campTime]);
+                } else {
+                    player.updateMovingAVG("AVGCAMP", "AVGCAMPN", campTime);
+                }
+            }
+        }
+    }
+    else if (text == "The Core entrance is opening!") {
+        getPartyMembers();
+        let termsTime = Date.now() - termsStart;
+        termsTime /= 1000;
+        termsTime = parseFloat( termsTime.toFixed(2) );
+
+        if (termsTime > 70) {
+            termsTime = 70;
+        }
+
+        for (let name of Object.keys(partyMembers)) {
+            let player = getPlayerDataByName(name);
+
+            if (!player) {
+                executeQueue.push([name, "updateMovingAVG", Date.now(), "AVGTERMS", "AVGTERMSN", termsTime]);
+            } else {
+                player.updateMovingAVG("AVGTERMS", "AVGTERMSN", termsTime);
+            }
+        }
+    }
+    else if (text == "[NPC] Mort: Here, I found this map when I first entered the dungeon.") {
+        runStart = Date.now();
+        getPartyMembers();
+    }
+    else if (text.match(/([a-zA-Z0-9_]{3,16}) completed a device!.+/)) {
+        let completedIn = parseFloat(((Date.now() - termsStart) / 1000).toFixed(2));
+        console.log(`completedin> ${completedIn}`);
+        const match = text.match(/([a-zA-Z0-9_]{3,16}) completed a device!.+/);
+        const name = match[1].toLowerCase();
+        let player = getPlayerDataByName(name);
+        console.log(`${name} >> ${partyMembers[name]} << ${completedIn}`)
+        if (completedIn > 17) {
+            completedIn = 17;
+        }
+
+        if (!ssDone && partyMembers[name] == "Healer") {
+            if (completedIn != 17) ChatLib.chat(`SS Completed in ${completedIn}`);
+            console.log(`ssDone Detected and SS Completed in ${completedIn}`);
+            ssDone = true;
+            console.log(`updating AVGSSTIME completedIn: ${completedIn}`);
+            if (!player) {
+                executeQueue.push([name, "updateMovingAVG", Date.now(), "AVGSSTIME", "AVGSSTIMEN", completedIn]);
+            } else {
+                player.updateMovingAVG("AVGSSTIME", "AVGSSTIMEN", completedIn);
+            }
+        }
+
+        if (!pre4Done && partyMembers[name] == "Berserk") {
+            if (completedIn != 17) ChatLib.chat(`Pre4 Completed in ${completedIn}`);
+            pre4Done = true;
+            if (!player) {
+                executeQueue.push([name, "PRE4", Date.now(), completedIn]);
+            } else {
+                player.playerData.PRE4RATEN += 1;
+                if (completedIn < 17) {
+                    player.playerData.PRE4RATE += 1;
+                }
+                player.save();
+            }
+        }
+    }
+}).setFilteredClass(S02PacketChat);
+
+
+register("command", (...args) => {
+    if (!args?.[0]) {
+        commandHelp();
+        return;
+    }
+  
+    switch (args[0]) {
+        case "help": {
+            commandHelp();
+            break;
+        }
+        case "autokick": {
+            data.autoKick = !data.autoKick;
+            data.save();
+            ChatLib.chat(`autokick ${data.autoKick ? "enabled" : "disabled"}`);
+            break;
+        }
+        case "sayreason": {
+            data.sayReason = !data.sayReason;
+            data.save();
+            ChatLib.chat(`say reason ${data.sayReason ? "enabled" : "disabled"}`);
+            break;
+        }
+        case "get":
+        case "view":
+        case "check":
+        case "see": {
+            if (!args[1] || args[1] === undefined) {
+                ChatLib.chat(`/big ${args[0]} username`);
+                return;
+            }
+            let player = getPlayerDataByName(args[1].toLowerCase());
+            if (!player) {
+                executeQueue.push([args[1].toLowerCase(), "PRINTPLAYER", Date.now()])
+            } else {
+                player.printPlayer();
+            }
+            break;
+        }
+        case "dodge": {
+            let username = args?.[1]?.toLowerCase();
+            let length = args?.[2];
+            let note;
+            if(!parseInt(length)) {
+                note = args?.splice(2)?.join(" ");
+            } else {
+                note = args?.splice(3)?.join(" ");
+            }
+
+            if (!username) {
+                ChatLib.chat("/big dodge <name> <days?> <note?>");
+                return;
+            }
+
+            let player = getPlayerDataByName(username);
+            if (!player) {
+                executeQueue.push[username, "dodge", Date.now(), length, note];
+            } else {
+                player.dodge(length, note);
+            }
+            break;
+        }
+        case "list":
+        case "viewall":
+        case "show":
+        case "all": {
+            printAll();
+            break;
+        }
+        default: {
+            let player = getPlayerDataByName(args[0]?.toLowerCase());
+            if (!player) {
+                executeQueue.push([args[0], "DEFAULT", Date.now(), args]);
+                return;
+            }
+            if(args.length > 2) {
+                let note = args?.splice(1)?.join(" ");
+                if (!note) note = " ";
+                player.playerData.NOTE = note;
+                player.save();
+            } else {
+                player.dodge(args?.[1]);
+            }
+            break;
+        }
+    }
+}).setName("big");
+
+
+const printAll = () => {
+    let fileNames = new File("./config/ChatTriggers/modules/bigtracker/players").list();
+    for (let i = 0; i < fileNames.length; i++) {
+        let player = new PlayerObject(fileNames[i].replace(".json", ""));
+        if (!player.playerData.DODGE && player.playerData.NOTE == "") continue;
+        let playerString = `${player.playerData.USERNAME}:`
+        if (player.playerData.NOTE !== "") {
+            playerString += ` ${player.playerData.NOTE}`;
+        }
+        if (player.playerData.DODGE) {
+            if (player.playerData.DODGELENGTH !== 0) {
+                let timeLeft = Date.now() - player.playerData.DODGEDATE;
+                timeLeft /= 1000; // seconds
+                timeLeft /= 60; // minutes
+                timeLeft /= 60; // hours
+                timeLeft /= 24; // days
+                timeLeft = parseFloat( (player.playerData.DODGELENGTH - timeLeft).toFixed(1) );
+                playerString += ` (dodged; ${timeLeft} days remaining)`;
+            } else {
+                playerString += ` (dodged)`;
+            }
+        }
+        ChatLib.chat(playerString);
+    }
+}
+
+
+
+let executeQueue = [];
+register("tick", () => {
+    for (let i = 0; i < executeQueue.length; i++) {
+        if (i < 0 || i > executeQueue.length) return;
+        if (!executeQueue?.[i]?.[2]) return;
+
+        if (Date.now() - executeQueue[i][2] > 5000) {
+            console.log(`failed to get player ${executeQueue[i][0]}`);
+            delete executeQueue[i];
+            continue;
+        }
+        let player = getPlayerDataByName(executeQueue[i][0], false);
+
+        if (!player) continue;
+
+        switch (executeQueue[i][1]) {
+            case "dodge": {
+                player.dodge(executeQueue[i]?.[3]);
+                break;
+            }
+            case "check": {
+                player.check(data.autoKick, data.sayReason);
+                break;
+            }
+            case "updateMovingAVG": {
+                player.updateMovingAVG(executeQueue[i][3], executeQueue[i][4], executeQueue[i][5]);
+                break;
+            }
+            case "DEATHS": {
+                player.playerData.DEATHS += 1;
+                player.save();
+                break;
+            }
+            case "PRE4": {
+                player.playerData.PRE4RATEN += 1;
+                let completedIn = executeQueue[i][3];
+                if (completedIn < 17) {
+                    player.playerData.PRE4RATE += 1;
+                }
+                player.save();
+                break;
+            }
+            case "PRINTPLAYER": {
+                player.printPlayer();
+                break;
+            }
+            case "DEFAULT": {
+                let args = executeQueue[i][3];
+                if(args.length > 2) {
+                    let note = args?.splice(1)?.join(" ");
+                    if (!note) note = " ";
+                    player.playerData.NOTE = note;
+                    player.save();
+                } else {
+                    player.dodge(args?.[1]);
+                }
+                break;
+            }
+        }
+        delete executeQueue[i];
+    }
+});
+
+
+
+const commandHelp = () => {
+    ChatLib.chat("/big help &7<- this");
+    ChatLib.chat("/big get <name> &7<- view stored info about a player");
+    ChatLib.chat("/big dodge <name> <days?> &7<- mark player as dodged. optionally add num of days to dodge the player for. dodge again to undodge.");
+    ChatLib.chat("/big list &7<- view all players with notes");
+    ChatLib.chat("/big <name> <note> &7<- add or remove a note about a player");
+    ChatLib.chat("/big autokick &7<- autokick dodged players");
+    ChatLib.chat("/big sayreason &7<- say note in chat when autokicking someone");
+    ChatLib.chat("/big sstimes &7<- print all players average ss times from fastest to slowest");
+    ChatLib.chat("/big dodge <name> <days?> <note?> &7<- shortcut for /big dodge");
+}
+
+
+const getSSTimes = () => {
+    let fileNames = new File("./config/ChatTriggers/modules/bigtracker/players").list();
+    let sortedSSTimes = [];
+    for (let i = 0; i < fileNames.length; i++) {
+        let player = new PlayerObject(fileNames[i].replace(".json", ""));
+        if (player.playerData.AVGSSTIMEN == 0) {
+            continue;
+        }
+        let ssTime = player.getSSMedian();
+        if (ssTime == 0.0) {
+            continue;
+        }
+        sortedSSTimes.push([player.playerData.USERNAME, ssTime, player.playerData.AVGSSTIME]);
+    }
+
+    sortedSSTimes.sort((a,b) => a[1] - b[1]);
+    sortedSSTimes.forEach(p => {
+        ChatLib.chat(`${p[0]}: ${p[1]} (${p[2]})`);
+    });
+}

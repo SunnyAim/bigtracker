8cc951a21a0dc4eb2d35be7e81730aa49729eb11
eatplastic <117698474+eatpIastic@users.noreply.github.com>
2025-03-25 12:37:15 -0500

    added info to import cheater list

--- index.js
+++ index.js
@@ -1190,19 +1190,24 @@ class BigCommand {
         request(`https://raw.githubusercontent.com/eatpIastic/list/refs/heads/main/uuids.txt`).then( (res) => {
             res = JSON.parse(res);
             let UUIDS = Object.keys(res);
+            let numDodges = 0;
+            let numNotes = 0;
             for (let i = 0; i < UUIDS.length; i++) {
                 let uuid = UUIDS[i];
                 let name = res[uuid];
                 let player = new BigPlayer(uuid, name);
                 if (!player.playerData?.["DODGE"]) {
                     player.playerData["DODGE"] = true;
+                    numDodges++;
                 }
                 if (!player.playerData?.["NOTE"] || player.playerData["NOTE"].trim() == "") {
                     player.playerData["NOTE"] = "cheater imported from list";
+                    numNotes++;
                 }
                 player.save();
             }
             ChatLib.chat(`&7> &asuccessfully imported &f${UUIDS.length} &acheaters from the list`);
+            ChatLib.chat(`&7> &aadded &f${numDodges} &adodges and &f${numNotes} &anotes`);
         }).catch( (e) => console.error(e));
     }
 
